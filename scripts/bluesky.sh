482ce19a96be6258ed4d56138e0053b870f2f85c
Alex Narvey <anarvey@precursor.ca>
2025-07-22 07:52:38 -0500

    Update bluesky.sh
    Added tests for VNCLOCALONLY and PASSWORDAUTHENTICATION

--- scripts/bluesky.sh
+++ scripts/bluesky.sh
@@ -9,6 +9,23 @@ OUTPUT_FILE="${CACHEDIR}bluesky.txt"
 SEPARATOR=' = '
 SERIAL=$(/usr/libexec/PlistBuddy -c "print :'serial'" /var/bluesky/settings.plist)
 VERSION=$(/usr/libexec/PlistBuddy -c "print :'version'" /var/bluesky/settings.plist)
+#TEST FOR VNCOnlyLocal
+if defaults read /Library/Preferences/com.apple.RemoteManagement.plist VNCOnlyLocalConnections
+&> /dev/null; then
+VNCLOCALONLY="0"
+else
+VNCLOCALONLY=$(defaults read /Library/Preferences/com.apple.RemoteManagement.plist VNCOnlyLocalConnections)
+fi
+#END TEST for VNCOnlyLocal
+#TEST FOR PASSWORDauth
+if grep -iqE '^\s*#?\s*PasswordAuthentication\b.*\byes\s*$' /etc/ssh/ssh_config; then
+    PASSWORDAUTH="1"
+elif grep -iqE '^\s*#?\s*PasswordAuthentication\b.*\bno\s*$' /etc/ssh/ssh_config; then
+    PASSWORDAUTH="0"
+else
+    # echo "No PasswordAuthentication line ending in yes/no found."
+fi
+#END TEST FOR PASSWORDauth
 
 # Test for BlueSky settings.plist exists
 if [ -f "/var/bluesky/settings.plist" ]; then
@@ -16,6 +33,8 @@ if [ -f "/var/bluesky/settings.plist" ]; then
 # Output data here
 echo "version${SEPARATOR}$VERSION" > ${OUTPUT_FILE}
 echo "serial${SEPARATOR}$SERIAL" >> ${OUTPUT_FILE}
+echo "vnclocalonly${SEPARATOR}$VNCLOCALONLY" >> ${OUTPUT_FILE}
+echo "passwordauth${SEPARATOR}$PASSWORDAUTH" >> ${OUTPUT_FILE}
 # END Output data
 
 else
