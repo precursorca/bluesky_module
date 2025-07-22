e17beda77d60c79441fe0a9111cec000a3fa7b45
Alex Narvey <anarvey@precursor.ca>
2025-07-22 08:03:42 -0500

    Update bluesky_model.php
    Added vnclocalonly and passwordauth plus some comments.

--- bluesky_model.php
+++ bluesky_model.php
@@ -9,8 +9,9 @@ class Bluesky_model extends Eloquent
     protected $hidden = ['id', 'serial_number'];
 
     protected $fillable = [
-      'serial_number',
-      'version',
-
+      'serial_number', //$this->rt['serial_number'] = 'VARCHAR(255) UNIQUE';
+      'version', // Version number of BlueSky software
+      'vnclocalonly', // State of VNCLocalOnlyConnections in com.apple.RemoteManagement.plist
+      'passwordauth', // State of PasswordAuthentication in etc/ssh/ssh_config
     ];
 }
